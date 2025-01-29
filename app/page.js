b1ebd232bedb7454fdeeeea50341908880ea3a61
andresListorti <andreslistorti@gmail.com>
2025-01-29 10:09:14 -0300

    meta

--- app/page.js
+++ app/page.js
@@ -15,6 +15,18 @@ export const metadata = {
     "Descubre la mejor colección de zapatos e indumentaria de cuero para hombre y mujer de Genaro. Últimas tendencias en calzado de calidad, estilo y confort.",
   keywords:
     "Zapatos de cuero, Indumentaria de cuero , Calzado de calidad, Moda de cuero, Calzado para hombre, Calzado para mujer, ultimas tendencias en calzado, Colección de zapatos, Estilo y confort, Accesorios de moda",
+  openGraph: {
+    title: "Genaro | Zapatos e Indumentaria de Cuero para Hombre y mujer",
+    description:
+      "Descubre la mejor colección de zapatos e indumentaria de cuero para hombre y mujer de Genaro. Últimas tendencias en calzado de calidad, estilo y confort.",
+    url: "",
+    siteName: "Genaro | Zapatos e Indumentaria de Cuero para Hombre y mujer",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dgiqb0ipg/image/upload/v1726159834/shoes-877939_1920_ghyeho.jpg",
+      },
+    ],
+  },
 };
 
 export default function Home() {
