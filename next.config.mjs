c840553dcd51e9f1e9156380cc202419dc1abf85
andresListorti <andreslistorti@gmail.com>
2026-01-20 23:53:47 -0300

    build-2

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    cpus: 1, // Reduces the number of threads to save memory
-    workerThreads: false,
+  // experimental: {
+  //   cpus: 1, // Reduces the number of threads to save memory
+  //   workerThreads: false,
+  // },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
   images: {
     remotePatterns: [
